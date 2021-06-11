5ccd3ad9d73b53d6922b19b960164cac384e14c3
Flavia <flavia_ferri@hotmail.com>
2021-06-11 12:15:41 +0200

    Fix map

--- src/components/MapId.jsx
+++ src/components/MapId.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, ZoomControl } from "react-leaflet";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 const Map = styled(MapContainer)(
-  () => css`
-    height: 100vh;
+  ({ theme }) => css`
+    height: 80vh;
     width: 100%;
     z-index: 0;
     position: absolute;
-    top: 0;
+    bottom: 0;
     z-index: -1;
+
+    ${theme.breakpoints[0]} {
+      height: 60vh;
+    }
   `
 );
 
@@ -19,14 +23,16 @@ const MapIp = ({ lat, lng }) => {
     <>
       <Map
         center={[lat || 51.505, lng || -0.09]}
-        zoom={15}
+        zoom={16}
         scrollWheelZoom={true}
+        zoomControl={false}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[lat || 51.505, lng || -0.09]}></Marker>
+        <ZoomControl position="bottomleft"></ZoomControl>
       </Map>
     </>
   );
